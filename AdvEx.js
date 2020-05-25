603259cd90d35e5c3370a9d9d85f9a22227d880a
Patrick Miglar <miglar.patrick@gmail.com>
2020-05-25 12:20:40 +0200

    finished

---
+++ AdvEx.js
@@ -0,0 +1,21 @@
+var a = 7;
+var b = 14;
+var c = 21;
+var d = 36;
+var e = 42;
+var sum = (a+b+c+d+e);
+
+document.write(sum);
+
+var f = 1;
+var g = 15;
+var h = 8;
+var i = 1;
+var mult = (f*g*h*i);
+
+document.write(mult);
+
+var div = (sum/mult);
+
+document.write(div);
+
---
+++ AdvEx2.js
@@ -0,0 +1,7 @@
+var multid = [ [1, 2, 1, 24], [8, 11, 9, 4], [7, 0, 7, 27], [7, 4, 28, 14], [3, 10, 25, 7], [21, 4, 6, 17], [3, 5, 26, 3] ]
+
+document.write(multid[1][1]+"<br>");
+document.write(multid[4][2]+"<br>");
+document.write(multid[5][3]+"<br>");
+document.write(multid[2][3]+"<br>");
+document.write(multid[2][1]);
